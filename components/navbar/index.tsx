d5138442edf4823e58cfe3402e18dda018e4d378
psxjoy <psxjoy@apache.org>
2025-04-29 12:40:20 +0800

    init

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./index.module.css";
-import { useRouter } from "nextra/hooks";
+import { useRouter } from 'next/router';
 import { useTranslation } from "../../hooks/useTranslation";
 
 const Navbar = () => {
